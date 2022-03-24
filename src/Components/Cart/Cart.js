c8cc7873f6d1934129fba8dbed994b9478bf84d5
sraboni-bhuiyan <bhuiyansraboni@gmail.com>
2022-03-24 21:50:17 +0100

    more changes added

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+    const {cart} = props;
+    
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+
+    }
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = total + shipping + tax;
     return (
-        <div className="cart-container">
+        <div className="cart">
             <h3>Order Summary</h3>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price: $</p>
-            <p>Total Shipping Charge: $</p>
-            <p>Tax: $</p>
-            <h3>Grand Total: $</h3>
-            <div className='btn-order'>
-                <button><p>Clear Cart</p></button>
-                <button><p>Review Order</p></button>
-            </div>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping Charge: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h4>Grand Total: ${grandTotal.toFixed(2)}</h4>
         </div>
     );
 };
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -31,7 +31,7 @@ const Shop = () => {
                     ></Product>)
                 }
             </div>
-            <div>
+            <div className='cart-container'>
                 <Cart cart={cart}></Cart>
             </div>
         </div>
