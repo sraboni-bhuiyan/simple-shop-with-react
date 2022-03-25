7345c5232091be3d1c8b061921ed786f94994e6c
sraboni-bhuiyan <bhuiyansraboni@gmail.com>
2022-03-25 09:42:39 +0100

    changes for local storage

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -14,12 +15,17 @@ const Shop = () => {
 
     } , []);
 
+    useEffect( () => {
+        const storedCart = getStoredCart();
+        console.log(storedCart);
+    } , []);
+
     const handleAddToCart = (product) => {
-        console.log(product);
+        
         //cart.push(product);
         const newCart = [...cart, product];
         setCart(newCart);
-        console.log(cart)
+        addToDb(product.id);
     }
     return (
         <div className='shop-container'>
